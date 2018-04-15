5de4a7f22a981a95da715b59f0307a1c26bf22f7
Dominik Broza <domibroz@gmail.com>
2018-04-15 17:53:52 +0200

    24.4 @Component in TaskRepository

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -1,25 +1,29 @@
 package com.crud.tasks.repository;
 
 import com.crud.tasks.domain.Task;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
 
-
+@Component
 public interface TaskRepository extends CrudRepository<Task, Long> {
+    @NotNull
     @Override
     List<Task> findAll();
 
+    @NotNull
     @Override
-    Optional<Task> findById(Long taskId);
+    Optional<Task> findById(@NotNull Long taskId);
+
+    @NotNull
+    @Override
+    Task save(@NotNull Task task);
 
     @Override
-    Task save(Task task);
-
-    @Override
-    void deleteById(Long taskId);
+    void deleteById(@NotNull Long taskId);
 
     @Override
     long count();
